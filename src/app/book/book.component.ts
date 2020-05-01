08cc1af4412695f3ce7e2e6dbd38a64ae23c5ad3
Andrew <betelgeuse.as@gmail.com>
2020-05-01 21:52:16 +0300

    book update

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -4,6 +4,10 @@ import { Subscription } from 'rxjs';
 
 import { Book } from '../data-access/entities/book.entity';
 import { DatabaseService } from '../data-access/database.service';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {NgbModal, NgbModalConfig} from '@ng-bootstrap/ng-bootstrap';
+import {ImageHero} from '../data-access/entities/image-hero.entity';
+import {ImageHeroPreview} from '../data-access/entities/image-hero-preview.entity';
 
 @Component({
   selector: 'app-book',
@@ -13,10 +17,13 @@ import { DatabaseService } from '../data-access/database.service';
 export class BookComponent implements OnInit {
 
   id: number;
+  form: FormGroup;
   book: Book;
   private subscription: Subscription;
 
   constructor(
+    config: NgbModalConfig,
+    private modalService: NgbModal,
     private databaseService: DatabaseService,
     private activateRoute: ActivatedRoute,
   ) {
@@ -25,7 +32,12 @@ export class BookComponent implements OnInit {
     this.getBookById(this.id);
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      name: new FormControl('', Validators.required),
+      description: new FormControl('', Validators.required)
+    });
+  }
 
   getBookById(bookId) {
     this.databaseService
@@ -39,5 +51,41 @@ export class BookComponent implements OnInit {
     //   .innerJoinAndSelect('book.heroes', 'heroes')
     //   .innerJoinAndSelect('heroes.imagePreview', 'imagePreview')
     //   .getMany();
+  }
+
+  updateBook() {
+    if (this.form.valid) {
+      let {name, description} = this.form.value;
+      const book = this.book;
+
+      book.name = name;
+      book.description = description;
+
+      this.databaseService
+        .connection
+        .then(() => book.save())
+        .then(() => {
+          this.getBookById(this.id);
+        })
+        .then(() => {
+          name = '';
+          description = '';
+        });
+
+      this.closeUpdateBookModal();
+    }
+  }
+
+  openUpdateBookModal(content) {
+    const {name, description} = this.book;
+    this.form.controls.name.setValue(name);
+    this.form.controls.description.setValue(description);
+
+    this.modalService.open(content, { size: 'lg' });
+  }
+
+  closeUpdateBookModal() {
+    this.form.reset();
+    this.modalService.dismissAll('Close update book.');
   }
 }
