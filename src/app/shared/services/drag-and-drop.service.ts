fe52ebf6005227f98bb4b0a236bb54bc08379b92
Andrew <betelgeuse.as@gmail.com>
2020-05-29 00:52:06 +0300

    drag service info

--- src/app/shared/services/drag-and-drop.service.ts
+++ src/app/shared/services/drag-and-drop.service.ts
@@ -8,6 +8,26 @@ import { getManager } from 'typeorm';
 })
 export class DragAndDropService {
 
+  // HTML:
+  // <ul cdkDropList (cdkDropListDropped)="dragDropEntities($event)">
+  //   <li *ngFor="let item of items" cdkDrag>
+  //     <h3>Header</h3>
+  //     <div cdkDragHandle>Drag</div>
+  //   <li>
+  // </ul>
+
+  // JS:
+  // import { CdkDragDrop } from '@angular/cdk/drag-drop';
+  // constructor(
+  //   private dragAndDropService: DragAndDropService
+  // ) {}
+  // dragDropEntities(event: CdkDragDrop<string[]>) {
+  //   this.dragAndDropService.dragDropEntities(event, this.books);
+  // }
+  // dragDropEntities(event: CdkDragDrop<string[]>) {
+  //   this.dragAndDropService.dragDropEntities(event, this.heroes);
+  // }
+
   dragDropEntities(event: CdkDragDrop<string[]>, entities) {
     moveItemInArray(entities, event.previousIndex, event.currentIndex);
 
