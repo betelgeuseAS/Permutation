7a16284c3a6f69aa977167401708e43f637601c1
Andrew <betelgeuse.as@gmail.com>
2020-09-23 23:26:10 +0300

    remove all handler

--- src/app/dialogs/remove-all-sheet/remove-all-sheet.component.ts
+++ src/app/dialogs/remove-all-sheet/remove-all-sheet.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatBottomSheetRef, MAT_BOTTOM_SHEET_DATA } from '@angular/material/bottom-sheet';
-import { getRepository } from 'typeorm';
 import { DOCUMENT } from '@angular/common';
+import { DatabaseService } from '../../data-access/database.service';
+import { ToastService } from 'angular-toastify';
 
 interface SheetData {
   entity: any;
 }
 
 @Component({
-  selector: 'app-remove-sheet',
+  selector: 'app-remove-all-sheet',
   templateUrl: './remove-all-sheet.component.html',
   styleUrls: ['./remove-all-sheet.component.sass']
 })
@@ -17,7 +18,9 @@ export class RemoveAllSheetComponent implements OnInit {
   constructor(
     @Inject(DOCUMENT) private document: Document,
     @Inject(MAT_BOTTOM_SHEET_DATA) public data: SheetData,
-    private bottomSheetRef: MatBottomSheetRef<RemoveAllSheetComponent>
+    private bottomSheetRef: MatBottomSheetRef<RemoveAllSheetComponent>,
+    private databaseService: DatabaseService,
+    private toastService: ToastService
   ) {}
 
   ngOnInit(): void {}
@@ -29,11 +32,23 @@ export class RemoveAllSheetComponent implements OnInit {
       return;
     }
 
-    const heroRepository = getRepository(entity);
-    // heroRepository.delete()
-    //   .then(() => {
-    //     //
-    //   });
+    this.databaseService
+      .connection
+      .then(async connection => {
+        await connection
+          .getRepository(entity)
+          .createQueryBuilder()
+          .delete()
+          .execute();
+      })
+      .then(() => {
+        this.bottomSheetRef.dismiss();
+
+        this.toastService.success('All records successfully deleted');
+      })
+      .catch(() => {
+        this.toastService.warn('Unable to delete records');
+      });
   }
 
   cancelHandler() {
